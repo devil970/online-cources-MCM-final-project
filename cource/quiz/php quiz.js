88eea4ca39f392fa04b0140dab8d2ecd4ca7e32b
Atharva Dhawane <57084914+devil970@users.noreply.github.com>
2025-01-16 19:08:02 +0530

    Add files via upload

---
+++ cource/quiz/php quiz.js
@@ -0,0 +1,15 @@
+document.getElementById('quiz-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    const answers = ['a','b','c','c','a','b','c','a','a','a']  // Replace with the correct answers for each question.
+    const formData = new FormData(this);
+    let score = 0;
+
+    for (let i = 1; i <= 20; i++) {
+        if (formData.get(`q${i}`) === answers[i - 1]) {
+            score += 2;
+        }
+    }
+
+    document.getElementById('score').innerHTML = `Your Score: <span>${score}</span>`;
+    document.getElementById('results').classList.remove('hidden');
+});
---
+++ cource/quiz/react quiz.js
@@ -0,0 +1,15 @@
+document.getElementById('quiz-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    const answers = ['a','a','a','a','b','c','b','a','a','c']  // Replace with the correct answers for each question.
+    const formData = new FormData(this);
+    let score = 0;
+
+    for (let i = 1; i <= 20; i++) {
+        if (formData.get(`q${i}`) === answers[i - 1]) {
+            score += 2;
+        }
+    }
+
+    document.getElementById('score').innerHTML = `Your Score: <span>${score}</span>`;
+    document.getElementById('results').classList.remove('hidden');
+});
